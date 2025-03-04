9f5e19fd2b319bdc3215229d7bc55c4ea070fff0
Nick Vintartas <nv2009123@gmail.com>
2025-03-04 11:20:46 -0600

    Initial Commit

---
+++ classesCafeNJV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  classesCafeNJV
+//
+//  Created by NICHOLAS VINTARTAS on 3/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ classesCafeNJVTests/classesCafeNJVTests.swift
@@ -0,0 +1,17 @@
+//
+//  classesCafeNJVTests.swift
+//  classesCafeNJVTests
+//
+//  Created by NICHOLAS VINTARTAS on 3/4/25.
+//
+
+import Testing
+@testable import classesCafeNJV
+
+struct classesCafeNJVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
